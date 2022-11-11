868259203bd16c30b461c9ded606f59c8c207e11
Kumarmangalam Mishra <63307990+mangalam-123@users.noreply.github.com>
2022-11-12 00:08:35 +0530

    done adding files
    Completed

---
+++ c1-versions.tf
@@ -0,0 +1,23 @@
+
+terraform {
+  
+  required_version = "~>1.0"
+  required_providers{
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraformtfstates-us-east-1"
+    key = "terraformtfstates-us-east-1/terraform.tfstates"
+    region = "us-east-1"
+    dynamodb_table = "terraform_table"
+  }
+}
+
+provider "aws"{
+  profile = "default"
+  region = var.aws_region
+}
